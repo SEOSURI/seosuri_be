1980fe1d8fa8b12c679614877e8e8bb19cfa5a6c
Ji-su-Shin <caujisu2020@gmail.com>
2023-03-13 10:23:26 +0900

    first commit

---
+++ src/main/java/com/onejo/seosuri/SeosuriApplication.java
@@ -0,0 +1,13 @@
+package com.onejo.seosuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeosuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeosuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onejo/seosuri/SeosuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onejo.seosuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeosuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
