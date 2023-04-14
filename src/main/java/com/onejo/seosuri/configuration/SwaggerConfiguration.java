27c64e35fc033cb525325a328f30ed51122ae46f
hyuun01 <hyuun1501@gmail.com>
2023-04-14 16:30:39 +0900

    feat: swagger base packagee 수정

--- src/main/java/com/onejo/seosuri/configuration/SwaggerConfiguration.java
+++ src/main/java/com/onejo/seosuri/configuration/SwaggerConfiguration.java
@@ -21,7 +21,7 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.springboot.api"))
+                .apis(RequestHandlerSelectors.basePackage("com.onejo.seosuri"))
                 .paths(PathSelectors.any())
                 .build();
     }
