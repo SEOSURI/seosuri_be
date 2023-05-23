44cbda9641fd9a169dc30b102f8bb58365237480
hyuun01 <hyuun1501@gmail.com>
2023-05-23 20:41:29 +0900

    feat: 문제생성 - 나이문제 - 주석 작업

--- src/main/java/com/onejo/seosuri/service/algorithm/VarElementary5th.java
+++ src/main/java/com/onejo/seosuri/service/algorithm/VarElementary5th.java
@@ -1,19 +1,45 @@
 package com.onejo.seosuri.service.algorithm;
 
 public class VarElementary5th {
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // 템플릿 생성시만 이용
+
+    // template
     public String content_template = "";
     public String explanation_template = "";
     public String answer_template = "";
+
+    // boolean 변수값 template 생성시 이용
+    public boolean[] useYear_ls;
+    public boolean[] useMult_ls;
+    public boolean[] useAddMinus_ls;
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // template 생성, 실제 문제 생성 시 이용
+
+    // 상황문장 유형id
     public int[] sentence_category_id_ls;
+
+    // sign
     public int[] var_sign_ls;  // input
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // 실제 문제 생성 시에만 이용
+
+    // min, max 범위
     public int[] var_min_value_ls; // input
     public int[] var_max_value_ls; // input
     public int[] name_var_min_value_ls;    // input
     public int[] name_var_max_value_ls;    // input
+
+    // 이름
     public String[] name_ls;
+
+    // 숫자값
     public int[] name_var_ls;   // 결과
     public int[] var_ls;   // 결과
-    public boolean[] useYear_ls;
-    public boolean[] useMult_ls;
-    public boolean[] useAddMinus_ls;
+
 }
