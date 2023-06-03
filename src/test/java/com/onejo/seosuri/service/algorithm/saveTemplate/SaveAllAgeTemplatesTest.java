49b58c409cf91f4392d938f51a606758305dc9ca
hyuun01 <hyuun1501@gmail.com>
2023-06-03 23:18:34 +0900

    feat: 문제생성 테스트

---
+++ src/test/java/com/onejo/seosuri/service/algorithm/saveTemplate/SaveAllAgeTemplatesTest.java
@@ -0,0 +1,14 @@
+package com.onejo.seosuri.service.algorithm.saveTemplate;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaveAllAgeTemplatesTest {
+
+    @Test
+    void saveAllTemplates() {
+
+        //SaveAllAgeTemplates saveAllAgeTemplates = new SaveAllAgeTemplates();
+    }
+}
