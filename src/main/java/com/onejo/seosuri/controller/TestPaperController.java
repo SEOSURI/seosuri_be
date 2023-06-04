536cadd7f4e8052a4b612517325d37b0c58c01d7
Shin-jisu <81508062+Ji-su-Shin@users.noreply.github.com>
2023-06-04 22:55:25 +0900

    Merge pull request #37 from SEOSURI/feat/pdf
    fix: 이메일 발송 get->post 변경

--- src/main/java/com/onejo/seosuri/controller/TestPaperController.java
+++ src/main/java/com/onejo/seosuri/controller/TestPaperController.java
@@ -45,7 +45,7 @@ public class TestPaperController {
     }
 
     @Operation(summary = "시험지 이메일 발송", description = "입력한 이메일로 시험지 전송")
-    @GetMapping("/email")
+    @PostMapping("/email")
     public BaseResponse<String> sendTestPaperEmail(@RequestBody EmailDto emailDto){
         try{
             testPaperService.sendEmail(emailDto.getEmail(), emailDto.getTestPaperId());
