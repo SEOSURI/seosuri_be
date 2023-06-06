f44eb2372680c5ac436e6d39e8af2c8d8a02b701
hyuun01 <hyuun1501@gmail.com>
2023-06-06 23:40:44 +0900

    feat: 식 계산

---
+++ src/main/java/com/onejo/seosuri/service/algorithm/problem/CalculateExpr.java
@@ -0,0 +1,85 @@
+package com.onejo.seosuri.service.algorithm.problem;
+
+import java.util.Stack;
+
+public class CalculateExpr {
+
+    public static int calculate(String expression) {
+        Stack<Integer> operandStack = new Stack<>();
+        Stack<Character> operatorStack = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+
+            if (c == '+' || c == '-' || c == '*') {
+                while (!operatorStack.isEmpty() && hasPrecedence(c, operatorStack.peek())) {
+                    char operator = operatorStack.pop();
+                    int operand2 = operandStack.pop();
+                    int operand1 = operandStack.pop();
+                    int result = performOperation(operator, operand1, operand2);
+                    operandStack.push(result);
+                }
+                operatorStack.push(c);
+            } else if (c == '(') {
+                operatorStack.push(c);
+            } else if (c == ')') {
+                while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
+                    char operator = operatorStack.pop();
+                    int operand2 = operandStack.pop();
+                    int operand1 = operandStack.pop();
+                    int result = performOperation(operator, operand1, operand2);
+                    operandStack.push(result);
+                }
+                operatorStack.pop(); // Pop '(' from the stack
+            } else if (Character.isDigit(c)) {
+                int operand = 0;
+                while (i < expression.length() && Character.isDigit(expression.charAt(i))) {
+                    operand = operand * 10 + (expression.charAt(i) - '0');
+                    i++;
+                }
+                i--;
+                operandStack.push(operand);
+            } else {
+                throw new IllegalArgumentException("잘못된 식입니다.");
+            }
+        }
+
+        while (!operatorStack.isEmpty()) {
+            char operator = operatorStack.pop();
+            int operand2 = operandStack.pop();
+            int operand1 = operandStack.pop();
+            int result = performOperation(operator, operand1, operand2);
+            operandStack.push(result);
+        }
+
+        if (operandStack.isEmpty()) {
+            throw new IllegalArgumentException("잘못된 식입니다.");
+        }
+
+        return operandStack.pop();
+    }
+
+    private static boolean hasPrecedence(char operator1, char operator2) {
+        if (operator2 == '(' || operator2 == ')') {
+            return false;
+        }
+        if (operator1 == '*' && (operator2 == '+' || operator2 == '-')) {
+            return false;
+        }
+        return true;
+    }
+
+    private static int performOperation(char operator, int operand1, int operand2) {
+        switch (operator) {
+            case '+':
+                return operand1 + operand2;
+            case '-':
+                return operand1 - operand2;
+            case '*':
+                return operand1 * operand2;
+            default:
+                throw new IllegalArgumentException("잘못된 연산자입니다.");
+        }
+    }
+
+}
---
+++ src/test/java/com/onejo/seosuri/service/algorithm/problem/CalculateExprTest.java
@@ -0,0 +1,51 @@
+package com.onejo.seosuri.service.algorithm.problem;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculateExprTest {
+
+    @Test
+    void calculate() {
+        String[] pm = new String[] {"+", "-"};
+        String mult = "*";
+        for(int var1 = 0; var1 < 100; var1++){
+            for(int var2 = 0; var2 < 100; var2++){
+                for(int var3 = 0; var3 < 4; var3++){
+                    for(String pORm: pm) {
+                        if(pORm == "+") {
+                            assertEquals(CalculateExpr.calculate(
+                                            "(" + String.valueOf(var1) + pORm + String.valueOf(var2) + ")" + "*" + String.valueOf(var3))
+                                    , (var1 + var2) * var3);
+                        } else if(pORm == "-"){
+                            assertEquals(CalculateExpr.calculate(
+                                            "(" + String.valueOf(var1) + pORm + String.valueOf(var2) + ")" + "*" + String.valueOf(var3))
+                                    , (var1 - var2) * var3);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    void calculateBracketTest() {
+        String p = "+";
+        String m = "-";
+        String mult = "*";
+        int var1 = 3;
+        int var2 = 9;
+        int var3 = 5;
+        int var4 = 2;
+        String exprStr = String.valueOf(var3)  + "*" + "(" + String.valueOf(var1) + p + String.valueOf(var2) + p + String.valueOf(var4) + ")" + "*" + String.valueOf(var3)  + "*" + String.valueOf(var3);
+        int exprInt = var3 * (var1 + var2 + var4) * var3 * var3;
+        for(int i = 0; i < 2; i++){
+            exprStr = String.valueOf(var3)  + "*" + "(" + String.valueOf(var1) + m + exprStr + m + String.valueOf(var2) + ")" + "*" + String.valueOf(var3)  + "*" + String.valueOf(var3);
+            exprInt = var3 * (var1 - exprInt - var2) * var3 * var3;
+            assertEquals(CalculateExpr.calculate(exprStr), exprInt);
+        }
+    }
+}
