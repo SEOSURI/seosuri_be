7a37ffb28ef8c121986fc8e098a748dc53ad2a96
hyuun01 <hyuun1501@gmail.com>
2023-06-08 22:54:44 +0900

    feat: ocr - multipartfile -> string 으로 api 변경

--- src/main/java/com/onejo/seosuri/controller/ClassificationController.java
+++ src/main/java/com/onejo/seosuri/controller/ClassificationController.java
@@ -13,6 +13,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -35,13 +37,13 @@ public class ClassificationController {
     private final ClassificationService classificationService;
 
     @Operation(summary = "문제 유형 분류", description = "사진을 넘겨주면 상위 유형 3개를 제시")
-    @PostMapping("")
-    public BaseResponse<List<CategoryResDto>> classification(@RequestBody ProblemPictureReqDto problemPictureReqDto){
+    @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public BaseResponse<String> classification(@RequestParam("file") MultipartFile multipartFile){
         try{
-            String ocrRes = classificationService.ocrImage(problemPictureReqDto.getFilePath());
-            System.out.println("\nOCR RESULT START ::\n"+ocrRes+"\n::OCR RESULT END\n");
-            List<CategoryResDto> categoryResDtos = classificationService.classification(ocrRes);
-            return new BaseResponse<>(categoryResDtos);
+            String ocrRes = classificationService.ocrImage(multipartFile);
+            //List<CategoryResDto> categoryResDtos = classificationService.classification(ocrRes);
+            //return new BaseResponse<>(categoryResDtos);
+            return new BaseResponse<>(ocrRes);
         } catch(BusinessException e) {
             return new BaseResponse<>(e.getErrorCode());
         }
--- src/main/java/com/onejo/seosuri/service/ClassificationService.java
+++ src/main/java/com/onejo/seosuri/service/ClassificationService.java
@@ -4,9 +4,14 @@ import com.onejo.seosuri.ai.orc.ImageToText;
 import com.onejo.seosuri.controller.dto.classification.CategoryResDto;
 import com.onejo.seosuri.domain.classification.CategoryTitle;
 import com.onejo.seosuri.exception.common.BusinessException;
+import com.onejo.seosuri.exception.common.ErrorCode;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -15,25 +20,29 @@ import java.util.Random;
 public class ClassificationService {
 
     @Transactional
-    public String ocrImage(String image_filename) throws BusinessException {
-        ImageToText imageToText = new ImageToText();
-        String ocr_res_str = null;
-        String filePath = image_filename;
-        /*
+    public String ocrImage(MultipartFile multipartFile) throws BusinessException {
         try {
-            InputStream tempInputStream = getClass().getResourceAsStream("/image/"+image_filename);
-            StringWriter writer = new StringWriter();
-            IOUtils.copy(tempInputStream, writer, "UTF-8");
-            //filePath = writer.toString();
-            //filePath = IOUtils.toString(getClass().getResourceAsStream("/image/"+image_filename), "UTF-8");
-            System.out.println("\nFILEPATH:: "+filePath+"\n");
-        } catch (IOException e) {
-            throw new BusinessException(ErrorCode.NO_EXIST_FILE);
+            File file = convert(multipartFile);
+            ImageToText imageToText = new ImageToText();
+            String ocr_res_str = imageToText.detectText(file);
+            return ocr_res_str;
+        } catch(IllegalStateException e){
+            throw new BusinessException(ErrorCode.MULTIPARTFILE_CONVERT_ERROR);
         }
+    }
 
-         */
-        ocr_res_str = imageToText.detectText(filePath);
-        return ocr_res_str;
+    public File convert(MultipartFile file)
+    {
+        File convFile = new File(file.getOriginalFilename());
+        try {
+            convFile.createNewFile();
+            FileOutputStream fos = new FileOutputStream(convFile);
+            fos.write(file.getBytes());
+            fos.close();
+        } catch (IOException e) {
+            throw new BusinessException(ErrorCode.FILE_IO_ERROR);
+        }
+        return convFile;
     }
 
     @Transactional
